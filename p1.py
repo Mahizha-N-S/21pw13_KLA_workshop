b086b0cdbc1c3a638a566ef3f70dc8aface8d3dd
Mahizha-N-S <102713447+Mahizha-N-S@users.noreply.github.com>
2023-06-23 16:36:40 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -28,6 +28,8 @@ with open('Level_1_Input_Data/input.json') as json_file:
     care_areas=care_areas[0]
     top_left=care_areas['top_left']
     bottom_right=care_areas['bottom_right']
+    width=data['die']['width']
+    height=data['die']['height']
     for x in range(top_left['x'],bottom_right['x']):
         for y in range(bottom_right['y'],top_left['y']):
             coordinate_list.append([x,y])
@@ -39,13 +41,13 @@ for i in coordinate_list:
     pix3.append(im3.getpixel(c))
     pix4.append(im4.getpixel(c))
     pix5.append(im5.getpixel(c))
-    
+  
 #checking for dfects
 def find_defect(j,image1,image2,image3):
     defect=[]
     for i in range(len(image1)):
         if image1[i]!=image2[i] and image1[i]!=image3[i]:
-            defect.append(coordinate_list[i])
+            defect.append((j,coordinate_list[i][0],height-coordinate_list[i][1]-1))
     return defect
            
 def1=find_defect(1,pix1,pix2,pix3)
@@ -54,20 +56,10 @@ def3=find_defect(3,pix3,pix4,pix5)
 def4=find_defect(4,pix4,pix5,pix1)
 def5=find_defect(5,pix5,pix1,pix2)
 
-#to add the die number
-def die_number(defect,number):
-    for i in range(len(defect)):
-        defect[i].insert(0,number)
-    return defect
-
-def1=die_number(def1,1)
-def2=die_number(def2,2)
-def3=die_number(def3,3)
-def4=die_number(def4,4)
-def5=die_number(def5,5)
 
 
-#write into csv files
+
+# #write into csv files
 
 with open('defect_coordinates.csv','w',newline='') as file:
     writer=csv.writer(file)
@@ -76,6 +68,7 @@ with open('defect_coordinates.csv','w',newline='') as file:
     writer.writerows(def3)
     writer.writerows(def4)
     writer.writerows(def5)
+
     
     
     
